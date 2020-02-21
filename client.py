cc338e78b637042bf6edf29bcfee5c0122c0f930
GurevichVarvara <gurevichvarvara@gmail.com>
2020-02-21 17:42:53 +0300

    Added queue of messages, reciever is separeted

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 
 UDP_CLIENT_ADDRESS = '127.0.0.1'
 UDP_SERVER_IP_ADDRESS = '127.0.0.1'
-UDP_SERVER_PORT = 1234
+UDP_SERVER_PORT = 5000
 SERVER_ADDRESS = (UDP_SERVER_IP_ADDRESS, UDP_SERVER_PORT)
 
 HEADER_SIZE = 8
@@ -21,8 +21,8 @@ while not is_received:
 	ans_from_server, address = client_socket.recvfrom(1024)
 	ans_from_server = ans_from_server.decode('utf-8')
 	if address == SERVER_ADDRESS:
-		if ans_from_server == 'received':
-			print('received')
+		if ans_from_server == 'sent':
+			print('sent')
 			received = True
 			break
 		else:
--- server.py
+++ server.py
@@ -1,39 +1,58 @@
 import socket
+import threading
+import queue
 
-UDP_SERVER_IP_ADDRESS = '127.0.0.1'
-UDP_SERVER_PORT = 1234
+def receive_message(socket, received_message_queue):
+    full_message = ''
+    is_new_message = True
 
-HEADER_SIZE = 8
-PACKAGE_SIZE = 10
+    while True:
+        part_of_message, address = socket.recvfrom(PACKAGE_SIZE)
+        part_of_message = part_of_message.decode('utf-8')
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((UDP_SERVER_IP_ADDRESS, UDP_SERVER_PORT))
+        # continue receiving message
+        if not is_new_message:
+            full_message += part_of_message
 
-print('Server running...')
+        # start receiving message
+        else:
+            message_len = int(part_of_message[:HEADER_SIZE])
+            is_new_message = False
+            full_message += part_of_message[HEADER_SIZE:]
 
-full_message = ''
-is_new_message = True
+        # send to client current receiving status
+        if len(full_message) == message_len:
+            socket.sendto(bytes('sent', 'utf-8'), address)
 
-while True:
-    part_of_message, address = server_socket.recvfrom(PACKAGE_SIZE)
-    part_of_message = part_of_message.decode('utf-8')
+            # add data from client to queue
+            received_message_queue.put((str(address[0]) + ' ' + str(address[1]), full_message))
 
-    # continue receiving message
-    if not is_new_message:
-        full_message += part_of_message
+            is_new_message = True
+            full_message = ''
+        else:
+            socket.sendto(bytes(str(HEADER_SIZE + len(full_message)) + ' ' + str(PACKAGE_SIZE), 'utf-8'), address)
 
-    # start receiving message
-    else:
-        message_len = int(part_of_message[:HEADER_SIZE])
-        is_new_message = False
-        full_message += part_of_message[HEADER_SIZE:]
 
-    # send to client current receiving status
-    if len(full_message) == message_len:
-        print('message from ' + str(address[0]) + str(address[1]) + ': ' + full_message)
-        is_new_message = True
-        full_message = ''
-        server_socket.sendto(bytes('received', 'utf-8'), address)
-    else:
-        server_socket.sendto(bytes(str(HEADER_SIZE + len(full_message)) + ' ' + str(PACKAGE_SIZE), 'utf-8'),
-                             address)
+def run_server(address, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((address, port))
+    print('Server running...')
+
+    received_message_queue = queue.Queue()
+    threading.Thread(target=receive_message, args=(server_socket, received_message_queue)).start()
+
+    while True:
+        while not received_message_queue.empty():
+            address, message = received_message_queue.queue[0][0], received_message_queue.queue[0][1]
+            received_message_queue.get()
+            print('Message from ' + address + ' is: ' + message)
+
+
+if __name__ == '__main__':
+    UDP_SERVER_IP_ADDRESS = '127.0.0.1'
+    UDP_SERVER_PORT = 5000
+
+    HEADER_SIZE = 8
+    PACKAGE_SIZE = 10
+
+    run_server(UDP_SERVER_IP_ADDRESS, UDP_SERVER_PORT)
