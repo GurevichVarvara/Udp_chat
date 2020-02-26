c030860ce0eb8aada56920eaf459c1ccee102773
GurevichVarvara <gurevichvarvara@gmail.com>
2020-02-26 14:25:10 +0300

    Added simultaneous sending and receiving. Added authorization

--- client.py
+++ client.py
@@ -1,76 +1,93 @@
 import socket
+import threading
+import queue
 
 HEADER_SIZE = 8
-PACKAGE_SIZE = 10
+PACKAGE_SIZE = 30
 
+def send_message(socket, message, receiving_status_message_queue):
+	message_to_send = f'{len(message):<{HEADER_SIZE}}' + message
+	socket.send(bytes(message_to_send, 'utf-8'))
 
-def send_message(socket, message):
-	message_to_received = f'{len(message):<{HEADER_SIZE}}' + message
-	print(message)
+	while True:
+		if not receiving_status_message_queue.empty():
+			message = receiving_status_message_queue.get()
 
-	socket.send(bytes(message_to_received, 'utf-8'))
-	is_received = False
-
-	while not is_received:
-		ans_from_server, address = socket.recvfrom(1024)
-		ans_from_server = ans_from_server.decode('utf-8')
-		if address == SERVER_ADDRESS:
-			if ans_from_server == 'sent':
-				print('sent')
-				break
+			if message == 'sent':
+				return True
 			else:
-				current_index_of_message, package_size = ans_from_server.split()[0], ans_from_server.split()[1]
-				socket.send(bytes(message_to_received[int(current_index_of_message):], 'utf-8'))
+				current_index_of_message = message
+				socket.send(bytes(message_to_send[int(current_index_of_message):], 'utf-8'))
 
 
-def receive_message(socket, received_message_queue):
-    full_message = ''
-    is_new_message = True
+def receive_message(socket, received_message_queue, receiving_status_message_queue, server_address):
+	full_message = ''
+	is_new_message = True
 
-    while True:
-        part_of_message, address = socket.recvfrom(PACKAGE_SIZE)
-        part_of_message = part_of_message.decode('utf-8')
+	while True:
+		part_of_message, address = socket.recvfrom(PACKAGE_SIZE)
+		part_of_message = part_of_message.decode('utf-8')
 
-        # continue receiving message
-        if not is_new_message:
-            full_message += part_of_message
+		# if sender is not server or message is showing receiving status
+		if address != server_address:
+			continue
 
-        # start receiving message
-        else:
-            message_len = int(part_of_message[:HEADER_SIZE])
-            is_new_message = False
-            full_message += part_of_message[HEADER_SIZE:]
+		# if receiving status message is received
+		if part_of_message[:HEADER_SIZE] == ' ' * HEADER_SIZE:
+			receiving_status_message_queue.put(part_of_message[HEADER_SIZE:])
+			continue
 
-        # send to client current receiving status
-        if len(full_message) == message_len:
-            socket.sendto(bytes('sent', 'utf-8'), address)
+		# continue receiving message
+		if not is_new_message:
+			full_message += part_of_message
 
-            # add data from client to queue
-            received_message_queue.put((str(address[0]) + ' ' + str(address[1]), full_message))
+		# start receiving message
+		else:
+			message_len = int(part_of_message[:HEADER_SIZE])
+			is_new_message = False
+			full_message += part_of_message
 
-            is_new_message = True
-            full_message = ''
-        else:
-            socket.sendto(bytes(str(HEADER_SIZE + len(full_message)) + ' ' + str(PACKAGE_SIZE), 'utf-8'), address)
+		# send to client current receiving status
+		if len(full_message[HEADER_SIZE:]) == message_len:
+			socket.send(bytes(' ' * HEADER_SIZE + 'sent', 'utf-8'))
+
+			# add data from client to queue
+			received_message_queue.put(full_message[HEADER_SIZE:])
+
+			is_new_message = True
+			full_message = ''
+		else:
+			socket.send(bytes(' ' * HEADER_SIZE + str(len(full_message)), 'utf-8'))
 
 
 def run_client(server_address):
 	client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	client_socket.connect(server_address)
 
+	received_message_queue = queue.Queue()
+	receiving_status_message_queue = queue.Queue()
+	threading.Thread(target=receive_message, args=(client_socket, received_message_queue, receiving_status_message_queue, server_address)).start()
+
+	name = input('Please type your name: ')
+	send_message(client_socket, name, receiving_status_message_queue)
+
+	# get the answer from server is typed name valid
 	while True:
-		data = input()
+		if not received_message_queue.empty():
+			message = received_message_queue.get()
+			if message == 'valid':
+				is_name_valid = True
+				break
+			elif message == 'not valid':
+				print('This name already exists.')
+				name = input('Please type your name: ')
 
-		if data == '':
-			continue
-
-		send_message(client_socket, data)
-
+	print('Hello, ' + name + '!')
 
 
 if __name__ == '__main__':
-	UDP_SERVER_IP_ADDRESS = '127.0.0.1'
-	UDP_SERVER_PORT = 5000
+	UDP_SERVER_IP_ADDRESS = '192.168.1.57'
+	UDP_SERVER_PORT = 5001
 	SERVER_ADDRESS = (UDP_SERVER_IP_ADDRESS, UDP_SERVER_PORT)
 	UDP_CLIENT_IP_ADDRESS = '127.0.0.1'
 
--- server.py
+++ server.py
@@ -3,35 +3,37 @@ import threading
 import queue
 
 HEADER_SIZE = 8
-PACKAGE_SIZE = 10
+PACKAGE_SIZE = 30
 
 
-def send_message(socket, message):
-    message_to_received = f'{len(message):<{HEADER_SIZE}}' + message
-    print(message)
+def send_message(socket, message, receiving_status_message_queue, address):
+    message_to_send = f'{len(message):<{HEADER_SIZE}}' + message
 
-    socket.send(bytes(message_to_received, 'utf-8'))
-    is_received = False
+    socket.sendto(bytes(message_to_send, 'utf-8'), address)
+    
+    while True:
+    	if not receiving_status_message_queue.empty():
+    		message = receiving_status_message_queue.get()
 
-    while not is_received:
-        ans_from_server, address = socket.recvfrom(1024)
-        ans_from_server = ans_from_server.decode('utf-8')
-        if address == SERVER_ADDRESS:
-            if ans_from_server == 'sent':
-                print('sent')
-                break
-            else:
-                current_index_of_message, package_size = ans_from_server.split()[0], ans_from_server.split()[1]
-                socket.send(bytes(message_to_received[int(current_index_of_message):], 'utf-8'))
+    		if message == 'sent':
+    			return True
+    		else:
+    			current_index_of_message = message
+    			socket.send(bytes(message_to_send[int(current_index_of_message):], 'utf-8'))
 
 
-def receive_message(socket, received_message_queue):
+def receive_message(socket, received_message_queue, receiving_status_message_queue):
     full_message = ''
     is_new_message = True
 
     while True:
         part_of_message, address = socket.recvfrom(PACKAGE_SIZE)
         part_of_message = part_of_message.decode('utf-8')
+        
+        # if receiving status message is received
+        if part_of_message[:HEADER_SIZE] == ' ' * HEADER_SIZE:
+        	receiving_status_message_queue.put(part_of_message[HEADER_SIZE:])
+        	continue
 
         # continue receiving message
         if not is_new_message:
@@ -41,19 +43,18 @@ def receive_message(socket, received_message_queue):
         else:
             message_len = int(part_of_message[:HEADER_SIZE])
             is_new_message = False
-            full_message += part_of_message[HEADER_SIZE:]
+            full_message += part_of_message
 
         # send to client current receiving status
-        if len(full_message) == message_len:
-            socket.sendto(bytes('sent', 'utf-8'), address)
+        if len(full_message[HEADER_SIZE:]) == message_len:
+        	socket.sendto(bytes(' ' * HEADER_SIZE + 'sent', 'utf-8'), address)
 
-            # add data from client to queue
-            received_message_queue.put((str(address[0]) + ' ' + str(address[1]), full_message))
-
-            is_new_message = True
-            full_message = ''
+        	# add data from client to queue
+        	received_message_queue.put((address, full_message))
+        	is_new_message = True
+        	full_message = ''
         else:
-            socket.sendto(bytes(str(HEADER_SIZE + len(full_message)) + ' ' + str(PACKAGE_SIZE), 'utf-8'), address)
+        	socket.sendto(bytes(' ' * HEADER_SIZE + str(len(full_message)), 'utf-8'), address)
 
 
 def run_server(address, port):
@@ -62,17 +63,34 @@ def run_server(address, port):
     print('Server running...')
 
     received_message_queue = queue.Queue()
-    threading.Thread(target=receive_message, args=(server_socket, received_message_queue)).start()
+    receiving_status_message_queue = queue.Queue()
+    threading.Thread(target=receive_message, args=(server_socket, received_message_queue, receiving_status_message_queue)).start()
+
+    user_addresses = set()
+    names = set()
+    user_names = {}
+    private_chats = {}
 
     while True:
         while not received_message_queue.empty():
             address, message = received_message_queue.queue[0][0], received_message_queue.queue[0][1]
             received_message_queue.get()
-            print('Message from ' + address + ' is: ' + message)
+
+            address_str = str(address[0]) + ':' + str(address[1])
+            print('Message from ' + address_str + ': ' + message)
+            
+            if address_str not in user_addresses:
+                user_addresses.add(address_str)
+                
+                if message not in names:
+                    names.add(message)
+                    send_message(server_socket, 'valid', receiving_status_message_queue, address)
+                else:
+                    send_message(server_socket, 'not valid', receiving_status_message_queue, address)
 
 
 if __name__ == '__main__':
-    UDP_SERVER_IP_ADDRESS = '127.0.0.1'
-    UDP_SERVER_PORT = 5000
+    UDP_SERVER_IP_ADDRESS = '192.168.1.57'
+    UDP_SERVER_PORT = 5001
 
     run_server(UDP_SERVER_IP_ADDRESS, UDP_SERVER_PORT)
